c0f6a8c96527c0363e710435f72617785810a5e3
nghiatk54 <nghiatk54@gmail.com>
2024-01-05 19:38:49 +0700

    Bai 2 search value in binary tree

--- day13/print-binary.cpp
+++ day13/print-binary.cpp
@@ -57,32 +57,91 @@ public:
 
         printPreorder(root->right);
     }
+
+    // Search value in binary tree
+    void searchValue(BinaryTree *root, int &value)
+    {
+        // Check binary tree empty
+        if (root == NULL)
+        {
+            cout << value << " is not in binary tree" << endl;
+            return;
+        }
+
+        // Check value = root
+        if (value == root->number)
+        {
+            cout << value << " is in binary tree" << endl;
+            return;
+        }
+
+        // Check value > root
+        else if (value > root->number)
+        {
+            searchValue(root->right, value);
+        }
+        else
+        {
+            searchValue(root->left, value);
+        }
+    }
 };
 
 int main()
 {
+    int process;
+    int flag = 0;
     int input;
     BinaryTree tree;
     BinaryTree *root = NULL;
 
-    cout << "Enter numbers (enter a negative number to stop): ";
-    while (cin >> input)
+    while (flag == 0)
     {
-        if (input < 0)
-            break;
+        cout << "*******************************\tENTER NUMBER TO CHECK HOMEWORK*******************************\t" << endl;
+        cout << "1. Build binary tree\t\t"
+             << "2. Search value in binary tree\t\t"
+             << "3. Exit" << endl;
+        cin >> process;
+        switch (process)
+        {
+        case 1:
+        {
+            cout << "Enter numbers (enter a negative number to stop): ";
+            while (cin >> input)
+            {
+                if (input < 0)
+                    break;
 
-        if (root == NULL)
-        {
-            root = tree.addNode(root, input);
+                if (root == NULL)
+                {
+                    root = tree.addNode(root, input);
+                }
+                else
+                {
+                    tree.addNode(root, input);
+                }
+            }
+
+            cout << "Number of binary tree: ";
+            tree.printPreorder(root);
+            break;
         }
-        else
+        case 2:
         {
-            tree.addNode(root, input);
+            // Check search value into user
+            cout << "Enter number search in binary tree: ";
+            int value;
+            cin >> value;
+            tree.searchValue(root, value);
+            break;
+        }
+        case 3:
+            flag = 1;
+            break;
+        default:
+            cout << "Invalid number to choice " << endl;
         }
     }
-
-    cout << "Number of binary tree: ";
-    tree.printPreorder(root);
 
     return 0;
 }
